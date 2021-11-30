9e30fed9c5de7f285d2eb107433771a8b541cd5f
Priyamesh Gupta <priyamesh28@gmai.com>
2021-11-30 11:37:04 +0530

    added animations for wnning and responsiveness

--- script.js
+++ script.js
@@ -15,13 +15,17 @@ const changeturn=()=>{
 //win logic
 const checkwin=()=>{
     let boxtexts=document.getElementsByClassName('boxtext');
-    let wins=[ [0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6] ];
+    let wins=[ [0,1,2,0,6,0], [3,4,5,0,18,0], [6,7,8,0,30,0], 
+                [0,3,6,12,18,90], [1,4,7,0,18,90], [2,5,8,12,18,90], 
+                [0,4,8,0,18,45], [2,4,6,0,18,135] ];
     wins.forEach((e)=> {
             if ((boxtexts[e[0]].innerText === boxtexts[e[1]].innerText) && (boxtexts[e[1]].innerText === boxtexts[e[2]].innerText) && (boxtexts[e[0]].innerText !== "")) {
                 document.querySelector('.info').innerHTML = `${boxtexts[e[0]].innerText} Won`;
                 console.log(boxtexts[e[0]].innerText);
                 isgameover = true;
                 document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width="200px";
+                document.querySelector('.line').style.width="36vw";
+                document.querySelector('.line').style.transform=`translate(${e[3]}vw,${e[4]}vw) rotate(${e[5]}deg)`;
             }
         });
 };
@@ -52,4 +56,5 @@ reset.addEventListener('click',()=>{
     isgameover=false;
     document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width="0px";
     document.getElementsByClassName('info')[0].innerHTML=`Turn for ${turn}`;
+    document.querySelector('.line').style.width="0vw";
 });
